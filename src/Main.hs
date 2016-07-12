b0f936544e5a24f4552b38a9acb6fcedf55c43eb
Christian Briones <cwbriones@gmail.com>
2016-07-11 23:04:27 -0700

    move environments to use IORef underneath
    Enable recursive bindings (and closures) to work properly by nesting
    environments within an IORef, to avoid problems with recursive lookup
    when the maps are immutable.
    
    At this point it's no longer clear if the state monad is needed other
    than for the sake of convenience, it is a bit misleading.

--- src/Main.hs
+++ src/Main.hs
@@ -14,7 +14,7 @@ import Parse
 import MalVal
 import Environment
 
-main = HL.runInputT HL.defaultSettings $ repl initEnv
+main = HL.runInputT HL.defaultSettings $ initEnv >>= repl
 
 repl :: MalEnv -> HL.InputT IO ()
 repl env = do
@@ -43,8 +43,10 @@ eval (List (Symbol "do":exprs))   = doForm exprs
 eval (List [Symbol "let*", List bindings, expr]) = get >>= letStar bindings expr
 eval (List [Symbol "def!", Symbol var, val]) = define var val
 eval (List [Symbol "fn*", List bindings, expr]) = makeLambda bindings expr
-eval val@(Symbol var) = get >>= \env ->
-    case find var env of
+eval val@(Symbol var) = do
+    env <- get
+    found <- liftIO $ find var env
+    case found of
         Nothing -> throwError $ UnresolvedSymbol var
         Just val -> return val
 eval val@(List []) = return val
@@ -53,20 +55,20 @@ eval val = return val
 
 apply :: [MalVal] -> MalIO MalVal
 apply (Func (Fn f):args) = f args
-apply (Lambda vars expr:args) = do
-    env <- get
+apply (Lambda env vars expr:args) = do
     checkArgs (length vars) (length args)
-    withinEnv (boundEnv env) (eval expr)
+    foldM_ bind env bindings
+    withinEnv env (eval expr)
   where
-    boundEnv e = foldl bind (extend e) (zip vars args)
-    bind e (var, val) = insert var val e
+    bindings = zip vars args
+    bind e (var, val) = liftIO $ insert var val e >> return e
     checkArgs a b = unless (a == b) $ throwError $ BadForm "Wrong number of arguments"
 apply (val:_) = throwError $ CannotApply val
 
 --
 -- Evaluates an action within the context of a temporary environment.
 --
-withinEnv :: Env MalVal   -- ^The environment to perform our action within
+withinEnv :: MalEnv       -- ^The environment to perform our action within
           -> MalIO MalVal -- ^The action to perform
           -> MalIO MalVal
 withinEnv newEnv action = do
@@ -87,8 +89,10 @@ withinEnv newEnv action = do
 -- Creates a lambda with the given parameter list and body.
 makeLambda :: [MalVal] -> MalVal -> MalIO MalVal
 makeLambda bindings expr = do
+    env <- get
+    childEnv <- liftIO $ extend env
     vars <- extractBindings bindings
-    return $ Lambda vars expr
+    return $ Lambda childEnv vars expr
   where
     extractBindings = mapM extract
     extract (Symbol s) = return s
@@ -131,7 +135,7 @@ define :: String -> MalVal -> MalIO MalVal
 define var val = do
     newVal <- eval val
     env <- get
-    put (insert var newVal env)
+    liftIO $ insert var newVal env
     return newVal
 
 -- let*
@@ -145,43 +149,46 @@ letStar :: [MalVal]      -- ^The list of bindings
         -> MalVal        -- ^The expression to evaluate
         -> MalEnv        -- ^The parent environment
         -> MalIO MalVal
-letStar bindings expr env =
+letStar bindings expr env = do
+    childEnv <- liftIO $ extend env
     withinEnv childEnv bindAndEval
   where
-    childEnv = extend env
     bindAndEval = evalBindings bindings >> eval expr
     evalBindings (Symbol var:expr:rest) = do
         val <- eval expr
         env <- get
-        put (insert var val env)
+        liftIO $ insert var val env
         evalBindings rest
     evalBindings [] = return ()
     evalBindings _  = throwError $ BadForm "invalid bindings list."
 
 {- Built-in Functions -}
 
-initEnv :: MalEnv
-initEnv = foldl (\env (var, f) -> insert var (Func (Fn f)) env) empty builtins
-    where builtins = [("+", binOp (+))
-                     ,("-", binOp (-))
-                     ,("*", binOp (*))
-                     ,("/", binOp Prelude.div)
-                     ,(">", compareOp (>))
-                     ,("<", compareOp (<))
-                     ,(">=", compareOp (>=))
-                     ,("<=", compareOp (<=))
-                     ,("=", equals)
-                     ,("list", list)
-                     ,("list?", isList)
-                     ,("count", count)
-                     ,("empty?", isEmpty)
-                     ]
+initEnv :: HL.InputT IO MalEnv
+initEnv =
+    liftIO empty >>= \env -> foldM defBuiltin env builtins
+  where
+    defBuiltin e (var, f) = liftIO $ insert var (Func (Fn f)) e >> return e
+    builtins = [("+", binOp (+))
+               ,("-", binOp (-))
+               ,("*", binOp (*))
+               ,("/", binOp Prelude.div)
+               ,(">", compareOp (>))
+               ,("<", compareOp (<))
+               ,(">=", compareOp (>=))
+               ,("<=", compareOp (<=))
+               ,("=", equals)
+               ,("list", list)
+               ,("list?", isList)
+               ,("count", count)
+               ,("empty?", isEmpty)
+               ]
 
-binOp :: (Int -> Int -> Int) -> [MalVal] -> MalIO MalVal
+binOp :: (Integer -> Integer -> Integer) -> [MalVal] -> MalIO MalVal
 binOp op [Number a, Number b] = return . Number $ op a b
 binOp _ _ = throwError BadArgs
 
-compareOp :: (Int -> Int -> Bool) -> [MalVal] -> MalIO MalVal
+compareOp :: (Integer -> Integer -> Bool) -> [MalVal] -> MalIO MalVal
 compareOp op [Number a, Number b] = return . Bool $ a `op` b
 compareOp _ _ = throwError BadArgs
 
@@ -194,7 +201,7 @@ isList [List _] = return (Bool True)
 isList [_] = return (Bool False)
 isList _ = throwError BadArgs
 
-count [List list] = return $ Number (length list)
+count [List list] = return $ Number $ toInteger . length $ list
 count [Nil] = return $ Number 0
 count _ = throwError BadArgs
 
